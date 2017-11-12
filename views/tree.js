337a9a2d55624a32376205c075d3e61d15eeeca9
DmitriySlabodchikov <32464960+DmitriySlabodchikov@users.noreply.github.com>
2017-11-13 02:03:59 +0300

    better

--- views/tree.js
+++ views/tree.js
@@ -1,7 +1,16 @@
 var tree = {
-		view:"tree",
-		id:"tree",
-		template:"#title#",
-		activeTitle:true,
-		data: treeInfo
+		
+	view:"treetable",
+	columns:[ 
+{ id:"id", header:"", width:50}, 
+{ id:"title", header:"Film title", width:250, 
+template:"{common.treetable()} #title#"}, 
+{ id:"price", header:"Price", width:200} 
+],
+
+	select:false,
+	autoheight:true,
+
+	data: treeInfo
+		
 };
