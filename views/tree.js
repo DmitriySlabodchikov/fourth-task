0bcb6dc9e700380b1d9d41cda6aab6c3f0f2eaee
DmitriySlabodchikov <32464960+DmitriySlabodchikov@users.noreply.github.com>
2017-11-13 02:04:40 +0300

    Update tree.js

--- views/tree.js
+++ views/tree.js
@@ -2,11 +2,11 @@ var tree = {
 		
 	view:"treetable",
 	columns:[ 
-{ id:"id", header:"", width:50}, 
-{ id:"title", header:"Film title", width:250, 
-template:"{common.treetable()} #title#"}, 
-{ id:"price", header:"Price", width:200} 
-],
+		{ id:"id", header:"", width:50}, 
+		{ id:"title", header:"Film title", width:250, 
+		template:"{common.treetable()} #title#"}, 
+		{ id:"price", header:"Price", width:200} 
+	],
 
 	select:false,
 	autoheight:true,
